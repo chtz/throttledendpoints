b4d6ab05a8dcd335b88422cec932645331ceab93
Christian Tschenett <cht@zuehlke.com>
2025-11-06 19:10:23 +0100

    initial

---
+++ src/main/java/ch/furthermore/poc/throttledendpoints/ThrottledendpointsApplication.java
@@ -0,0 +1,20 @@
+package ch.furthermore.poc.throttledendpoints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class ThrottledendpointsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThrottledendpointsApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello() {
+	    return "world";
+	}
+}
