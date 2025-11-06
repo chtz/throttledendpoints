bc3e467daaa7f5bdaaee9587e25cf9eac9d08b95
Christian Tschenett <cht@zuehlke.com>
2025-11-06 22:56:07 +0100

    endpoint caching example added

--- src/main/java/ch/furthermore/poc/throttledendpoints/ThrottledendpointsApplication.java
+++ src/main/java/ch/furthermore/poc/throttledendpoints/ThrottledendpointsApplication.java
@@ -1,10 +1,19 @@
 package ch.furthermore.poc.throttledendpoints;
 
+import java.util.OptionalLong;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cache.JCacheManagerCustomizer;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.github.benmanes.caffeine.jcache.configuration.CaffeineConfiguration;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -12,30 +21,48 @@ import jakarta.servlet.http.HttpServletRequest;
 @RestController
 @SpringBootApplication
 public class ThrottledendpointsApplication {
+    private final static Logger log = LoggerFactory.getLogger(ThrottledendpointsApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(ThrottledendpointsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ThrottledendpointsApplication.class, args);
+    }
 
-	/**
-	 * Sample 1: All endpoints and all source IPs share one global rate-limit bucket.
-	 * <pre>
-	 * config:
-	 * rate-limits:
+    @Bean
+    JCacheManagerCustomizer caffeineCustomizer() {
+        return cm -> {
+            var c = new CaffeineConfiguration<Object, Object>();
+            c.setMaximumSize(OptionalLong.of(100_000L));
+            c.setExpireAfterWrite(OptionalLong.of(java.time.Duration.ofSeconds(5).toNanos()));
+            cm.createCache("endpoints", c);
+
+            c = new CaffeineConfiguration<Object, Object>();
+            c.setMaximumSize(OptionalLong.of(10_000L));
+            c.setExpireAfterAccess(OptionalLong.of(java.time.Duration.ofHours(1).toNanos()));
+            cm.createCache("rate-limit-buckets", c);
+        };
+    }
+
+    /**
+     * Sample 1: All endpoints and all source IPs share one global rate-limit bucket.
+     * 
+     * <pre>
+     * config:
+     * rate-limits:
      *   - bandwidths:
      *     - capacity: 5
      *       time: 10
      *       unit: seconds
-	 * 
-	 * throttling (~every 2nd request):
-	 * watch -n 1 curl -v http://localhost:6363/
-	 * 
-	 * no throttling:
-	 * watch -n 2 curl -v http://localhost:6363/
-	 * </pre>
-	 * 
-	 * Sample 2: All endpoints share one global rate-limit bucket, instantiated per source IP.
-	 * <pre>
+     * 
+     * throttling (~every 2nd request):
+     * watch -n 1 curl -v http://localhost:6363/
+     * 
+     * no throttling:
+     * watch -n 2 curl -v http://localhost:6363/
+     * </pre>
+     * 
+     * Sample 2: All endpoints share one global rate-limit bucket, instantiated per source IP.
+     * 
+     * <pre>
      * config:
      * rate-limits:
      *   - cache-key: "getRemoteAddr()"
@@ -55,16 +82,19 @@ public class ThrottledendpointsApplication {
      * no throttling:
      * watch -n 2 curl -v http://192.168.1.11:6363/  # no throttling
      * watch -n 2 curl -v http://localhost:6363/     # no throttling
-	 * </pre>
-	 */
-	@GetMapping("/")
-	public String hello(HttpServletRequest request) {
-	    String clientIp = request.getRemoteAddr();
-	    return "world 1 - " + clientIp;
-	}
-	
-	@GetMapping("/h2")
+     * </pre>
+     */
+    @GetMapping("/")
+    public String hello(HttpServletRequest request) {
+        log.info("GET /");
+        String clientIp = request.getRemoteAddr();
+        return "world 1 - " + clientIp;
+    }
+
+    @Cacheable(cacheNames = "endpoints", key = "'/h2'")
+    @GetMapping("/h2")
     public String hello2(HttpServletRequest request) {
+        log.info("GET /h2");
         String clientIp = request.getRemoteAddr();
         return "world 2 - " + clientIp;
     }
